c97fdd77c04a8641f7ab755c803750210d31333d
Varshitha <123124066+Varsshitha@users.noreply.github.com>
2023-03-29 16:26:25 -0500

    modified

---
+++ routes/computation.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+let x;
+let y;
+router.get('/', function(req, res, next) {
+  //res.render('computation', { title: 'Maheshwar Punyam Anand' });
+  
+if(req.query.x ==undefined ) {
+  x=Math.floor(Math.random()*10);
+  y=Math.fround(Math.random());
+let y1=Math.pow(x,y);
+let y2=Math.sign(y);
+let y3=Math.tanh(x);
+  res.send('Math.pow() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.sign() applied to '+y+' is '+y2+"<br>"+'Math.tanh()  applied to '+x+' is'+y3); 
+
+}
+else{
+
+  x = req.query.x;
+  let y1=Math.pow()(x,x);
+  let y2=Math.sign(x);
+  let y3=Math.tanh(x);
+
+  res.send('Math.pow() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.sign() applied to '+y+' is '+y2+"<br>"+'Math.tanh()  applied to '+x+' is'+y3);
+}
+});
+
+module.exports = router;
